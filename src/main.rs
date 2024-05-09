a6b8e6311164350096db676e852674115df52220
Juan <ramisjuan1@gmail.com>
2024-05-09 07:07:39 +0200

    initial commit: native balance, object tokenErc20 balance

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use anyhow::{Ok, anyhow};
+use ethers_core::types::BlockId;
+use ethers_providers::{Http, Provider};
+use revm::{
+    db::{CacheDB, EthersDB},
+    primitives::{address, Address, TransactTo, U256, ExecutionResult, Output},
+    Database, Evm,
+};
+use std::sync::Arc;
+use alloy_sol_types::{sol, SolCall, SolValue};
+
+
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let mut db = get_db();
+
+    let account: Address = address!("0000000000000000000000000000000000000000");//address!("f39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+    //let _ = get_balance(&mut db, account).await;
+    let token = ERC20Token::new(address!("888888848B652B3E3a0f34c96E00EEC0F3a23F72"));
+
+    println!("Account balance: {:#?}", token.balance_of(account, &mut db)?.to_string());
+    Ok(())
+
+}
+
+
+fn get_db() -> CacheDB<EthersDB<Provider<Http>>> {
+    let client = Arc::new(Provider::<Http>::try_from(
+        "http://127.0.0.1:8545",
+    ).expect("Could not connect to the client"));
+    CacheDB::new(EthersDB::new(client.clone(), None/*Some(BlockId::from(19791181))*/).unwrap())
+}
+
+async fn get_balance(db: &mut CacheDB<EthersDB<Provider<Http>>>, account: Address) -> anyhow::Result<()> {
+    let mut evm = Evm::builder()
+        .with_db(db)
+        .build();
+
+    let account_info = evm.db_mut().basic(account)?;
+
+    println!("Account address: {:#?}", account.to_string());
+    //println!("Account nonce: {:#?}", account_info.unwrap().nonce.to_string());
+    //println!("Account code hash: {:#?}", account_info.unwrap().code_hash.to_string());
+    //println!("Account code: {:#?}", account_info.unwrap().code);
+    //println!("Account storage: {:#?}", account_info.unwrap());
+    println!("Account balance: {:#?}", account_info.unwrap().balance.to_string());
+    Ok(())
+}
+
+pub struct ERC20Token {
+    address : Address
+}
+
+impl ERC20Token {
+    pub fn new(address: Address) -> Self {
+        Self {
+            address
+        }
+    }
+
+    pub fn balance_of(&self, account: Address, db: &mut CacheDB<EthersDB<Provider<Http>>>) -> anyhow::Result<U256> {
+        sol! {
+            function balanceOf(address account) external view returns (uint);
+        }
+
+        let encoded = balanceOfCall { account }.abi_encode();
+
+        let mut evm = Evm::builder()
+            .with_db(db)
+            .modify_tx_env(|tx| {
+                tx.caller = account;
+                tx.transact_to = TransactTo::Call(self.address);
+                tx.data = encoded.into();
+                tx.value = U256::from(0);
+            })
+            .build();
+
+        let ref_tx = evm.transact_commit().unwrap();
+        let balance: U256 = match ref_tx {
+            ExecutionResult::Success {
+                output: Output::Call(value),
+                ..
+            } => <U256>::abi_decode(&value, false)?,
+            result => return Err(anyhow!("'balanceOf' execution failed: {result:?}")),
+        };
+
+        Ok(balance)
+    }
+}
